cab2a94168319934339c3a4f89070a2c71f67838
VChes <106433641+VChes@users.noreply.github.com>
2022-05-29 09:57:48 +0300

    Add files via upload

---
+++ lesson-1-1.py
@@ -0,0 +1,18 @@
+a = "It's a string!"
+b = 5
+c = False
+d = ["Ches" , "V" , 32, ["boom" , "boom"]]
+e = ("zip", 32, True)
+f = {"f-name":"V", "l-name":"Ches", "age":"33", "todo":"learn"}
+print(a)
+print(b)
+print(d[0])
+d[3] = ["bass"]
+print(d)
+print("issue below")
+# e[2] = 40
+print("is:"+f["age"])
+
+if f["todo"] != "learn":
+    print("you need to learn")
+
